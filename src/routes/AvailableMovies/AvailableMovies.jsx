e58ee10be099fcf7d9cf03faa91c6b83121dac77
Gabriel <gamcode98@gmail.com>
2023-09-02 23:05:44 -0300

    changes

--- src/routes/AvailableMovies/AvailableMovies.jsx
+++ src/routes/AvailableMovies/AvailableMovies.jsx
@@ -1,11 +1,12 @@
-import { Backdrop, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Container, Grid, Typography } from '@mui/material'
+import { Backdrop, Box, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Container, Grid, Stack, TextField, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { moviesWithoutCinemaShows } from '../../services/moviesWithoutCinemaShows'
-import { PencilIcon, TrashIcon } from '../../components/Icons'
+import { ArrowsRightLeftIcon, PencilIcon, SearchIcon, TrashIcon } from '../../components/Icons'
 
 export const AvailableMovies = () => {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [value, setValue] = useState('')
 
   useEffect(() => {
     moviesWithoutCinemaShows()
@@ -16,6 +17,18 @@ export const AvailableMovies = () => {
       .catch(err => console.log(err))
       .finally(() => setIsLoading(false))
   }, [])
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const filteredMovies = movies.filter(movie => {
+    return movie.title.toLowerCase().includes(value.toLowerCase())
+  })
+
+  const handleReset = () => {
+    setValue('')
+  }
 
   return (
     <Container>
@@ -35,8 +48,61 @@ export const AvailableMovies = () => {
       >
         Películas disponibles
       </Typography>
+      <Stack spacing={2} direction='row' sx={{ mb: '2rem', width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <TextField
+          id='search-movie'
+          label='Película a buscar'
+          variant='outlined'
+          value={value}
+          onChange={(e) => handleInputChange(e)}
+          InputProps={{
+            endAdornment: (
+              <SearchIcon />
+            )
+          }}
+        />
+
+        <Button
+          variant='outlined'
+          startIcon={<ArrowsRightLeftIcon />}
+          sx={{ textTransform: 'initial', fontSize: '1rem' }}
+          // onClick={handleSelectItem}
+        >
+          Cambiar a películas con funciones
+        </Button>
+      </Stack>
+
       <Grid container spacing={2}>
-        {!isLoading && movies.map(movie => (
+        {!isLoading && filteredMovies.length === 0 &&
+          <Box sx={{ mx: 'auto', display: 'flex', flexDirection: 'column' }}>
+            <Typography
+              variant='h3' component='h1' sx={{
+                textAlign: 'center',
+                marginY: '2rem',
+                fontWeight: 'bold',
+                fontSize: '2.5rem',
+                mx: 'auto'
+              }}
+            >
+              No se encontraron películas disponibles a la búsqueda
+            </Typography>
+            <Button
+              onClick={handleReset}
+              variant='contained' size='large'
+              sx={{
+                textTransform: 'initial',
+                fontWeight: '700',
+                fontSize: '1rem',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                display: 'block'
+              }}
+            >
+              Obtener todas las películas
+            </Button>
+          </Box>}
+
+        {!isLoading && filteredMovies.map(movie => (
           <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
             <Card>
               <CardMedia
